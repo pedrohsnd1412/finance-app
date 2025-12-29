77ebd7923a1ea2b7c90f8a4a36e384b717c0e4df
Pedro Henrique <pedrohsnd1@gmail.com>
2025-12-29 19:02:28 -0300

    feat: add connect screen to navigation stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -56,6 +56,7 @@ function RootLayoutNav() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="connect" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
   );
